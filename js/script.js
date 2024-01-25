40251f9528b60851f5b77ff6b639321f927b71e7
yaakoub96 <yaakoubdiafi@gmail.com>
2024-01-25 02:41:44 +0100

    Make playRound function to compare  player selection and computer selection and alert who is the winner

--- js/script.js
+++ js/script.js
@@ -8,3 +8,26 @@ const playerSelection = prompt(
   `please select one "rock , paper, scissors"`
 ).toLowerCase();
 console.log("Player selection is:", playerSelection);
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == "rock" && computerSelection == "scissors") {
+    alert("You Win! rock beasts scissors");
+  } else if (playerSelection == "rock" && computerSelection == "paper") {
+    alert("You Lose! paper beat rock");
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    alert("You Win! paper beat rock");
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    alert("You Lose! scissors beat paper");
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    alert("You Win! scissors beat paper");
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    alert("You Lose! rock beat scissors");
+  } else if (playerSelection == computerSelection) {
+    alert("You are equals");
+  } else if (playerSelection != selection) {
+    alert("you picked wrong selection");
+  }
+}
+
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
